6e05daedba5385e558d4248bee6b207f3e693e8e
kjh-busan <picsmash7@gmail.com>
2023-09-03 20:34:10 +0900

    auth

--- code/11-reading-and-dispatching/src/App.js
+++ code/11-reading-and-dispatching/src/App.js
@@ -6,7 +6,6 @@ import Header from './components/Header';
 import Auth from './components/Auth';
 import UserProfile from './components/UserProfile';
 
-
 function App() {
   const isAuth = useSelector(state => state.auth.isAuthenticated);
 
